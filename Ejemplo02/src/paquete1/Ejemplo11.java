00a07c8e7b8fdb92e33d2a3de21794f1841eadba
daangamarca2 <arturoangamarca120@gmail.com>
2025-10-30 10:32:22 -0500

    Asignatura

---
+++ Ejemplo02/src/paquete1/Ejemplo11.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo11 {
+
+    public static void main(String[] args) {
+        // Ingreso de valores por teclado
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US); // para ingresar los decimales con 
+                                        // punto
+                                        // no con coma
+        // declarar las variables
+        String nombreAsignatura1;        
+        double notaAsignatura1;
+        
+        
+        System.out.println("Ingrese nombre de asignatura 1");
+        nombreAsignatura1 = entrada.nextLine(); // nextLine me permite recibir o almacenar una cadena 
+        System.out.println("Ingrese nota de asignatura 1");
+        notaAsignatura1 = entrada.nextDouble();
+        
+        
+        System.out.printf("Asignatura 1: %s\nNota 1:%.2f" ,             
+                nombreAsignatura1,
+                notaAsignatura1
+                );
+    }
+    
+}
